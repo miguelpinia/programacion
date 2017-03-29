87e6aaa1a2bdb5e96bae0c181aa8608f9c90a2a6
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-03-29 09:05:46 -0600

    Ejemplo de clase.

--- practicas/soluciones/2/src/practica-2.py
+++ practicas/soluciones/2/src/practica-2.py
@@ -57,6 +57,9 @@ class Fuego(Pokemon):
     def bola_de_fuego(self):
         return randint(0, 10)
 
+    def ataques(self):
+        return "- lanzallamas\n-bola-de-fuego"
+
 
 class Electrico(Pokemon):
 
@@ -77,6 +80,74 @@ class Chispa(Fuego, Electrico):
 
     def chispazo(self):
         return randint(0, 15)
+
+####################
+# Jugador          #
+# ================ #
+# pokemon1         #
+# pokemon2         #
+# ================ #
+####################
+
+
+class Jugador(object):
+
+    def __init__(self, pokemon1, pokemon2):
+        self.pokemon1 = pokemon1
+        self.pokemon2 = pokemon2
+
+
+class JugadorNP(Jugador):
+
+    def __init__(self, pokemon1, pokemon2):
+        super(JugadorNP, self).__init__(pokemon1, pokemon2)
+
+    def ataque(self):
+        pass
+
+
+class Menu(object):
+
+    def __init__(self):
+        self.lista_pokemon = []
+
+    def bienvenida(self):
+        print('Pokemon Python 3\n')
+        print('Este es un juego de consola experimental\n')
+        print('Por favor se paciente con su uso\n')
+
+    def menu_inicial(self):
+        pass
+
+
+def genera_pokemon():
+    l_nombres = [
+        'bulbasaur',
+        'charizard',
+        'charmander',
+        'squirtle',
+        'pikachu',
+        'raichu']
+    l_pokemons = [Fuego, Electrico, Chispa]
+    i_nombre = randint(0, len(l_nombres) - 1)
+    i_pokemon = randint(0, len(l_pokemons) - 1)
+    p = l_pokemons[i_pokemon]
+    return p(l_nombres[i_nombre])
+
+
+def genera_pokemon_fuego():
+    nombres = ['charmander', 'charmeleon', 'charizard']
+    i_nombre = randint(0, len(nombres) - 1)
+    return Fuego(nombres[i_nombre])
+
+for i in range(10):
+    p = genera_pokemon_fuego()
+    print(p, type(p))
+
+for i in range(10):
+    p = genera_pokemon()
+    print(p, type(p))
+
 
 ########################################################
 # Zona de pruebas de código. Esta sección desaparecerá #
