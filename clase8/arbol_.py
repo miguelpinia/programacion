ccae006e5157b8c17da910e711e4dfbfcc9bc397
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2017-05-08 09:06:12 -0500

    Agrega cambios.

--- clase8/arbol_.py
+++ clase8/arbol_.py
@@ -54,6 +54,43 @@ class ArbolBinario(object):
             raiz.derecho = self.__insert(x, raiz.derecho)
             return raiz
 
+    def __busqueda(self, x, raiz):
+        if raiz is None:
+            return None
+        if raiz.valor == x:
+            return raiz
+        elif x < raiz.valor:
+            return self.__busqueda(x, raiz.izquierdo)
+        elif x > raiz.valor:
+            return self.__busqueda(x, raiz.derecho)
+
+    def busqueda(self, x):
+        return self.__busqueda(x, self.raiz)
+
+    def remove(self, x):
+        nodo = self.busqueda(x)
+        izquierdo = nodo.izquierdo
+        derecho = nodo.derecho
+
+    def __remove(self, x, raiz):
+        if raiz is not None:
+            if raiz.valor == x:
+                pass
+            elif x < raiz.valor:
+                if raiz.izquierdo.valor == x:
+                    aEliminar = raiz.izquierdo
+                    aEliminarIzquierdo = aEliminar.izquierdo
+                    aEliminarDerecho = aEliminar.derecho
+                    raiz.izquierdo = aEliminarDerecho
+                    minimo = aEliminarDerecho.izquierdo
+                    while minimo.izquierdo is not None:
+                        minimo = minimo.izquierdo
+                    minimo.izquierdo = aEliminarIzquierdo
+                else:
+                    return self.__remove(x, raiz.izquierdo)
+            elif raiz.valor > x:
+                pass
+
 
 def print_arbol(arbol):
     def imprime(raiz, profundidad):
@@ -62,3 +99,16 @@ def print_arbol(arbol):
             print(('\t' * profundidad) + str(raiz.valor))
             imprime(raiz.izquierdo, profundidad + 1)
     imprime(arbol.raiz, 1)
+
+arbol = ArbolBinario()
+arbol.insert(9)
+arbol.insert(4)
+arbol.insert(2)
+arbol.insert(5)
+arbol.insert(1)
+arbol.insert(3)
+arbol.insert(6)
+arbol.insert(11)
+arbol.insert(10)
+arbol.insert(12)
+print_arbol(arbol)
